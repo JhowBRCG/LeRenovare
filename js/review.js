b3c5b1688a00721afc61e2bbd54b84cb10ad9926
Jhonathan <guarino90@gmail.com>
2023-05-27 15:09:04 -0300

    Reform reviews-strcture

--- js/review.js
+++ js/review.js
@@ -1,48 +1,290 @@
 const reviews = [
   {
     id: 1,
-    img: "../assets/reviews/r1.png",
+    img: "../assets/reviews/img.png",
+    personName: "Talita Oliveira",
+    avaliationTimes: "3 avaliações",
+    dateReview: "3 meses atrás",
+    contentReview:
+      "Profissional extremamente competente,melhor massagem que fiz na vida,muito atencionsa, energia incrível, procura sempre entender o paciente e oferecer o melhor atendimento! Recomentdo para todos. ",
   },
 
   {
     id: 2,
-    img: "../assets/reviews/r2.png",
+    img: "../assets/reviews/img-2.png",
+    personName: "Cristina",
+    avaliationTimes: "2 avaliações",
+    dateReview: "10 meses atrás",
+    contentReview: "A melhor massoterapeuta que eu já conheci",
   },
 
   {
     id: 3,
-    img: "../assets/reviews/r3.png",
+    img: "../assets/reviews/img-3.png",
+    personName: "Stephanny Vasquez",
+    avaliationTimes: "1 avaliações",
+    dateReview: "10 meses atrás",
+    contentReview: `Lugar incrível!! <br>
+      Leticia lenotti a melhor massoterapeuta`,
   },
 
   {
     id: 4,
-    img: "../assets/reviews/r4.png",
+    img: "../assets/reviews/img-4.png",
+    personName: "leozin",
+    avaliationTimes: "1 avaliações",
+    dateReview: "10 meses atrás",
+    contentReview:
+      "Lugar tranquilo,ambiente agradável e a melhor massagem que eu ja tive! ",
   },
 
   {
     id: 5,
-    img: "../assets/reviews/r5.png",
+    img: "../assets/reviews/img-5.png",
+    personName: "beatrys barbosa",
+    avaliationTimes: "4 avaliações",
+    dateReview: "10 meses atrás",
+    contentReview: `Ótimo atendiment,nunca vi igual. <br>
+      sai renovada,votarei em breve com toda certeza,continue sempre assim trabalhando com muito amor!!`,
   },
 
   {
     id: 6,
-    img: "../assets/reviews/r6.png",
+    img: "../assets/reviews/img-6.png",
+    personName: "Maria Ivete Cardoso Carrasco",
+    avaliationTimes: "1 avaliações",
+    dateReview: "10 meses atrás",
+    contentReview:
+      "Minha experiência foi ótima! Massoterapeuta incrível,muito atenciosa e simpática. Melhor massagem que já fiz,indico para todos! Parabéns",
   },
   {
     id: 7,
-    img: "../assets/reviews/r7.png",
+    img: "../assets/reviews/img-7.png",
+    personName: "Karina Iris Rabello Marques",
+    avaliationTimes: "1 avaliações",
+    dateReview: "9 meses atrás",
+    contentReview: "Lugar e profissionais sensacionais",
   },
   {
     id: 8,
-    img: "../assets/reviews/r8.png",
+    img: "../assets/reviews/img-8.png",
+    personName: "Silverman Oliveira",
+    avaliationTimes: "4 avaliações",
+    dateReview: "9 meses atrás",
+    contentReview:
+      "Comprometimento e excelência no seu atendimento. Muito talentosa e sem duvidas a melhor profissional que já pude ter o prazer de ser cliente.",
   },
   {
     id: 9,
-    img: "../assets/reviews/r9.png",
+    img: "../assets/reviews/img-9.png",
+    personName: "Elaine Ramos",
+    avaliationTimes: "2 avaliações",
+    dateReview: "8 meses atrás",
+    contentReview: "Em poucas palavras... a melhor massagem da vida!",
   },
   {
     id: 10,
-    img: "../assets/reviews/r10.png",
+    img: "../assets/reviews/img-10.png",
+    personName: "Leonardo Côrte",
+    avaliationTimes: "1 avaliações",
+    dateReview: "8 meses atrás",
+    contentReview: "Experiência incrível! Muito atenciosa,adorei!!!",
+  },
+
+  {
+    id: 11,
+    img: "../assets/reviews/img-11.png",
+    personName: "Elaine Santos",
+    avaliationTimes: "2 avaliações",
+    dateReview: "3 meses atrás",
+    contentReview:
+      "Atendimento excelente! Ambiente muito agradável,recomendo muito!",
+  },
+
+  {
+    id: 12,
+    img: "../assets/reviews/img-12.png",
+    personName: "Felipe Santos",
+    avaliationTimes: "2 avaliações",
+    dateReview: "3 meses atrás",
+    contentReview:
+      "Local muito calmo e profissional super atenciosa! Voltarei mais vezes, super recomendo!",
+  },
+
+  {
+    id: 13,
+    img: "../assets/reviews/img-13.png",
+    personName: "Camila Bastos",
+    avaliationTimes: "2 avaliações",
+    dateReview: "2 semanas atrás",
+    contentReview:
+      "Lugar maravilhoso,a melhor massagem q já fiz na minha vida,a profissional super dedicada,atendimento impecável,entende o q está fazendo,isso é magnífico,parabéns Letícia vc é demais....",
+  },
+
+  {
+    id: 14,
+    img: "../assets/reviews/img-14.png",
+    personName: "Larissa Sayuri",
+    avaliationTimes: "3 avaliações",
+    dateReview: "8 meses atrás",
+    contentReview: `Ambiente incrível,melhor massagem que já tive na vida. <br>
+    Profissional super atenciosa.`,
+  },
+
+  {
+    id: 15,
+    img: "../assets/reviews/img-15.png",
+    personName: "Beatriz Barros",
+    avaliationTimes: "3 avaliações",
+    dateReview: "7 meses atrás",
+    contentReview: "Uma excelente massoterapeuta,atendimento excelente!",
+  },
+
+  {
+    id: 16,
+    img: "../assets/reviews/img-16.png",
+    personName: "Camilly Lima",
+    avaliationTimes: "1 avaliações",
+    dateReview: "7 meses atrás",
+    contentReview:
+      "Fiz massagem com a Letícia Lenotti,e foi tudo de bom,bem relaxante,muito atenciosa,com certeza vou fazer mais vezes!!",
+  },
+
+  {
+    id: 17,
+    img: "../assets/reviews/img-17.png",
+    personName: "Paulo Rodrigues",
+    avaliationTimes: "",
+    dateReview: "6 meses atrás",
+    contentReview: "",
+  },
+
+  {
+    id: 18,
+    img: "../assets/reviews/img-18.png",
+    personName: "Vithória Miranda",
+    avaliationTimes: "3 avaliações",
+    dateReview: "um ano atrás",
+    contentReview:
+      "Foi tudo ótimo. Quem me atendeu foi a Letícia e ela foi um amor de pessoa! Super recomendo.",
+  },
+
+  {
+    id: 19,
+    img: "../assets/reviews/img-19.png",
+    personName: "Ana clara rocha",
+    avaliationTimes: "2 avaliações",
+    dateReview: "um ano atrás",
+    contentReview: "Muito bom,o atendimento da Letícia foi espetacular!!",
+  },
+
+  {
+    id: 20,
+    img: "../assets/reviews/img-20.png",
+    personName: "Victoria Luna",
+    avaliationTimes: "3 avaliações",
+    dateReview: "um ano atrás",
+    contentReview:
+      "Atendimento de ótima qualidade,local aconchegante e com uma energia incrível! Gostaria de parabenizar a Letícia,que tem um atendimento excelente,acolhedor. Super simpática e atenciosa!! Uma experiência única,com certeza voltarei mais vezes.",
+  },
+
+  {
+    id: 21,
+    img: "../assets/reviews/img-21.png",
+    personName: "Laura Santos",
+    avaliationTimes: "1 avaliações",
+    dateReview: "um ano atrás",
+    contentReview:
+      "Lugar maravilhoso com profissional ótima,fui atendida com a Letícia e estou apaixonada! Delicada,simpática,sem dúvidas de excelente qualidade!",
+  },
+
+  {
+    id: 22,
+    img: "../assets/reviews/img-22.png",
+    personName: "Beatrys Barbosa",
+    avaliationTimes: "4 avaliações",
+    dateReview: "um ano atrás",
+    contentReview: `Foi sensacional o tendimento da profissional Letícia,cheguei destrída e sai de lá renovada,amei tudo. <br>
+    Voltarei com certeza.`,
+  },
+
+  {
+    id: 23,
+    img: "../assets/reviews/img-23.png",
+    personName: "Mariana Barreto Joao",
+    avaliationTimes: "1 avaliações",
+    dateReview: "um ano atrás",
+    contentReview:
+      "Adorei o lugar! Atendimento muito bom,fui atendida pela Letícia,super simpática e atenciosa! Faz uma massagem incrível! Com toda certeza voltarei mais vezes!",
+  },
+
+  {
+    id: 24,
+    img: "../assets/reviews/img-24.png",
+    personName: "Luyane Matos",
+    avaliationTimes: "6 avaliações",
+    dateReview: "10 meses atrás",
+    contentReview:
+      "Eu amooo um ótimo atendimento e é exatamente o que encontrei na clínica. Eu já tinha visto as boas avaliações e o instagram então decidi ter minha primeira experiência com a massagem com pedras quentes. Eu estou apaixonada simplesmente maravilhoso e a Letícia que fez minha massagem é simplesmente maravilhosa... eu amei demais e vou voltar com certeza.",
+  },
+
+  {
+    id: 25,
+    img: "../assets/reviews/img-25.png",
+    personName: "Glaucia Macedo",
+    avaliationTimes: "5 avaliações",
+    dateReview: "10 meses atrás",
+    contentReview: `Massagem ótima,lugar traquilo e aconchegante <br>
+    A Letícia é maravilhosa...super atenciosa e delicada! Super recomendo!!`,
+  },
+
+  {
+    id: 26,
+    img: "../assets/reviews/img-26.png",
+    personName: "Fátima Prado",
+    avaliationTimes: "2 avaliações",
+    dateReview: "9 meses atrás",
+    contentReview:
+      "Adoro o espaço acolhedor e o preparo das pessoas que nos atendem. Sou atendida pela Letícia,que além de acolher muito bem está sempre disposta a buscar inovações e fazer a diferença no seu trabalho. Além de outras pessoas maravilhosas que compõem a equipe,como Tati,que estão sempre disposta a resolver os nossos problemas com prontidão.",
+  },
+
+  {
+    id: 27,
+    img: "../assets/reviews/img-27.png",
+    personName: "Cris Massayo",
+    avaliationTimes: "2 avaliações",
+    dateReview: "9 meses atrás",
+    contentReview: `Estive pela primeira vez no espaço nessa semana,para o SPA Bem Estar e por 2 horas fiquei totalmente relaxada,mente vazia e aproveitando aquele momento para mim.<br>
+      O atendimento da Letícia foi excelente do início ao finally.`,
+  },
+
+  {
+    id: 28,
+    img: "../assets/reviews/img-28.png",
+    personName: "Ronaldo Brito",
+    avaliationTimes: "1 avaliações",
+    dateReview: "2 meses atrás",
+    contentReview: `Profissional Letícia excelente no que faz,Massagem super relaxante,ambiente agradável. Nota 10!`,
+  },
+
+  {
+    id: 29,
+    img: "../assets/reviews/img-29.png",
+    personName: "Josefa Garcia",
+    avaliationTimes: "1 avaliações",
+    dateReview: "um mês atrás",
+    contentReview:
+      "Adorei,vou continuar,fiz massoterapia para dor muscular,Dra tem mãos de fada,super recomendo.Gratidão.",
+  },
+
+  {
+    id: 30,
+    img: "../assets/reviews/img-30.png",
+    personName: "Cristina Lenotti",
+    avaliationTimes: "1 avaliações",
+    dateReview: "um ano atrás",
+    contentReview:
+      "Experiência ótima! Fui super bem atendida,ambiente com uma energia incrível,exelentes profissionais! Fui atendida pela massoterapeuta Letícia e foi maravilhosa minha sessão,melhor massagem da minha vida! Super recomendo!",
   },
 ];
 
@@ -50,6 +292,10 @@ const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 
 const reviewImg = document.querySelector(".review-img");
+const nameR = document.querySelector(".name");
+const avaliation = document.querySelector(".reviews");
+const date = document.querySelector(".date");
+const content = document.querySelector(".content");
 
 let currentItem = 0;
 
@@ -60,6 +306,10 @@ window.addEventListener("DOMContentLoaded,", function () {
 function showReview(t) {
   const item = reviews[t];
   reviewImg.src = item.img;
+  nameR.textContent = item.personName;
+  avaliation.textContent = item.avaliationTimes;
+  date.textContent = item.dateReview;
+  content.innerHTML = item.contentReview;
 }
 
 nextBtn.addEventListener("click", () => {
